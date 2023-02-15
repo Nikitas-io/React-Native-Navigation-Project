213931211978ce8633108211cdb9d7f1874ba646
Nikitas I/O <chrononautic@gmail.com>
2023-02-15 22:20:38 +0200

    Installed react navigation components and started adding it to the code

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 import { StyleSheet } from 'react-native';
 import CategoryScreen from './screens/CategoryScreen';
+import { StatusBar } from 'expo-status-bar';
+
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create a stack of screens.
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <CategoryScreen />
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          {/* A screen managed by the stack navigator. */}
+          <Stack.Screen name="MealsCategories" component={CategoryScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
