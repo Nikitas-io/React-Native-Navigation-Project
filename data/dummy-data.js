0649f29116f8b5e49574ae21e4e716b0aba62557
Nikitas I/O <chrononautic@gmail.com>
2023-02-16 19:04:23 +0200

    Some image URIs simply do not work

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -21,7 +21,7 @@ export const MEALS = [
     'Spaghetti with Tomato Sauce',
     'affordable',
     'simple',
-    'http://images.unsplash.com/photo-1598866594230-a7c12756260f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1908&q=80',
+    'https://cdn.pixabay.com/photo/2018/07/18/19/12/pasta-3547078_1280.jpg',
     20,
     [
       '4 Tomatoes',
